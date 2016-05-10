0ac63cf14b8a5c5f1dc4dc1f7b1a17e0f14d6d1d
Michele Palmia <micpalmia@gmail.com>
2016-05-10 14:12:04 +0200

    Add synonym support

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Elasticsearch concatenation token filter
-Elasticsearch plugin providing the `concatenate` token filter for merging tokens in a token stream back into one single token. The Lucene code is taken from a [discussion thread](http://elasticsearch-users.115913.n3.nabble.com/Is-there-a-concatenation-filter-td3711094.html) on the Elasticsearch mailing list.
+Elasticsearch plugin providing the `concatenate` token filter for merging tokens in a token stream back into one single token. 
+The Lucene code is taken from a [discussion thread](http://elasticsearch-users.115913.n3.nabble.com/Is-there-a-concatenation-filter-td3711094.html) on the Elasticsearch mailing list.
+
+This version of the concatenation token filter works with synonyms creating parallel token streams.
 
 ## Usage
 The plugin provides a token filter of type `concatenate`. The only provided parameter is `token_separator`, that allows to set the separator to use when concatenating the tokens back together.
 
-The `master` branch currently works with **Elasticsearch 1.5**.
+The `master` branch currently works with **Elasticsearch 2.3.2**.
 
 ## Example usage
 
@@ -19,12 +22,19 @@ The `master` branch currently works with **Elasticsearch 1.5**.
       "custom_stop" : {
         "type": "stop",
         "stopwords": ["and", "is", "the"]
+      },
+      "custom_synonyms" : {
+        "type" : "synonym",
+        "synonyms" : [
+            "universe => cosmos, space"
+        ]
       }
     },
     "analyzer" : {
       "stop_concatenate" : {
         "filter" : [
           "custom_stop",
+          "custom_synonyms"
           "concatenate"
         ],
         "tokenizer" : "standard"
@@ -33,18 +43,24 @@ The `master` branch currently works with **Elasticsearch 1.5**.
   }
 }
 ```
-This custom analyzer will first tokenize the input string using the `standard` tokenizer, will apply the stopwords removal (and any other filter we put before the `concatenate` filter), and finally concatenate all tokens back together using the provided `token_separator`.
+This custom analyzer will first tokenize the input string using the `standard` tokenizer, will apply the stopwords removal filter, the synonym filter (and any other filter we put before the `concatenate` filter), and finally concatenate all tokens back together using the provided `token_separator`. When a synonym is encountered, a second output token is created.
 
 ```bash
-POST /test/_analyze?text=the fox jumped over the fence&analyzer=stop_concatenate
+POST /test/_analyze?text=the universe is immense&analyzer=stop_concatenate
 {
   "tokens": [
     {
-      "token": "this is a test",
-      "start_offset": 0,
-      "end_offset": 0,
-      "type": "word",
-      "position": 1
+        "token": "cosmos immense",
+        "start_offset": 0,
+        "end_offset": 0,
+        "type": "SYNONYM",
+        "position": 1
+    }, {
+        "token": "space immense",
+        "start_offset": 0,
+        "end_offset": 0,
+        "type": "SYNONYM",
+        "position": 1
     }
   ]
 }
--- src/main/java/com/eyeem/elasticsearch/ConcatenateFilter.java
+++ src/main/java/com/eyeem/elasticsearch/ConcatenateFilter.java
@@ -3,40 +3,123 @@ package com.eyeem.elasticsearch;
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.util.AttributeSource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public final class ConcatenateFilter extends TokenFilter {
 
     private final static String DEFAULT_TOKEN_SEPARATOR = " ";
 
-    private final CharTermAttribute charTermAtt = addAttribute(CharTermAttribute.class);
+    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    private final PositionIncrementAttribute posIncrAtt = addAttribute(PositionIncrementAttribute.class);
+    
+    private int incrementGap = 100;
+    private AttributeSource.State previousState = null;
+    private boolean recheckPrevious = false;
 
     private String tokenSeparator = null;
-    private StringBuilder builder = new StringBuilder();
+    private LinkedList<StringBuilder> builders = new LinkedList<>();
+    private List<StringBuilder> buffers = new ArrayList<>();
+
+    public ConcatenateFilter(TokenStream input, String tokenSeparator, int incrementGap) {
+        super(input);
+        this.tokenSeparator = tokenSeparator!=null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
+        this.incrementGap = incrementGap;
+    }
 
     public ConcatenateFilter(TokenStream input, String tokenSeparator) {
         super(input);
         this.tokenSeparator = tokenSeparator!=null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
+        this.incrementGap = 100;
     }
 
     @Override
     public boolean incrementToken() throws IOException {
-        boolean result = false;
-        builder.setLength(0);
-        while (input.incrementToken()) {
-            if (builder.length()>0) {
-                // append the token separator
-                builder.append(tokenSeparator);
-            }
-            // append the term of the current token
-            builder.append(charTermAtt.buffer(), 0, charTermAtt.length());
+        if (builders.size() > 0) {
+            clearAttributes();
+            termAtt.setEmpty().append(builders.removeFirst());
+            posIncrAtt.setPositionIncrement(0);
+            return true;
         }
-        if (builder.length()>0) {
-            charTermAtt.setEmpty().append(builder);
 
-            result = true;
+        builders.clear();
+        buffers.clear();
+
+        if(recheckPrevious) {
+            restoreState(previousState);
+            // append the term of the current token
+            buffers.add(this.fromTermAttribute(termAtt));
+            recheckPrevious = false;
         }
-        return result;
+
+        while (input.incrementToken()) {
+            if (posIncrAtt.getPositionIncrement() == 0) {
+                buffers.add(this.fromTermAttribute(termAtt));
+            } else if(posIncrAtt.getPositionIncrement() <= incrementGap) {
+                this.expandBuilders(); // ONLY WORKS IF BUFFERS HAS ELEMENTS
+                buffers.add(this.fromTermAttribute(termAtt));
+            } else {
+                this.expandBuilders();
+
+                // we have found a new element in the array, the next token should start from this one
+                recheckPrevious = true;
+                previousState = captureState();
+                break;
+            }
+        }
+
+        // in case there are unexpanded buffers
+        // a) last token is a synonym
+        // b) last token has been replayed
+        this.expandBuilders();
+
+        if (builders.size() > 0) {
+            termAtt.setEmpty().append(builders.removeFirst());
+            posIncrAtt.setPositionIncrement(1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private StringBuilder fromTermAttribute(CharTermAttribute termAtt) {
+        StringBuilder n = new StringBuilder();
+        n.append(termAtt.buffer(), 0, termAtt.length());
+        return n;
+    }
+
+    private void expandBuilders() {
+        // if no buffers, nothing to do
+        if (this.buffers.size() > 0) {
+            // first buffers to be expanded
+            if (this.builders.isEmpty()) {
+                builders.add(new StringBuilder());
+            }
+
+            int sizeFirst = this.builders.size();
+            ListIterator<StringBuilder> iter = this.builders.listIterator();
+            for (int i = 0; i < sizeFirst; i++) {
+                // pop the next element
+                StringBuilder el = new StringBuilder(iter.next());
+                iter.remove();
+
+                for (StringBuilder buffer : buffers) {
+                    StringBuilder n = new StringBuilder(el);
+                    if (n.length() > 0) {
+                        n.append(this.tokenSeparator);
+                    }
+                    n.append(buffer);
+                    iter.add(n);
+                }
+            }
+
+            this.buffers.clear();
+        }
     }
 }
